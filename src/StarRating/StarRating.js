d2419454973eb0474897257ced20bcb1880965b7
Brian Barasa <briannbig@gmail.com>
2023-12-11 12:19:34 +0300

    Sample consuming of the refactored StarRating Component

--- src/StarRating/StarRating.js
+++ src/StarRating/StarRating.js
@@ -6,11 +6,11 @@ const Star = ({ selected = false, onSelect = f => { console.log("star clicked");
 );
 const createArray = length => [...Array(length)];
 
-export default function StarRating({ totalStars = 5 }) {
+export default function StarRating({ totalStars = 5, style = {}, ...props }) {
 
-    const [selectedStars, setSelectedStars] = useState(3);
+    const [selectedStars, setSelectedStars] = useState(0);
 
-    return <div>
+    return <div style={{ padding: "5px", ...style }} {...props}>
         {createArray(totalStars).map((n, i) => <Star key={i} selected={selectedStars > i} onSelect={() => {
             setSelectedStars(i + 1)
         }} />)}
