548aace6517494a4cfa18daa3aad496252e8f284
Brian Barasa <briannbig@gmail.com>
2023-12-11 11:54:23 +0300

    Update selected stars when a star is clicked

--- src/StarRating/StarRating.js
+++ src/StarRating/StarRating.js
@@ -8,10 +8,12 @@ const createArray = length => [...Array(length)];
 
 export default function StarRating({ totalStars = 5 }) {
 
-    const [selectedStars] = useState(3);
+    const [selectedStars, setSelectedStars] = useState(3);
 
     return <div>
-        {createArray(totalStars).map((n, i) => <Star key={i} selected={selectedStars > i} />)}
+        {createArray(totalStars).map((n, i) => <Star key={i} selected={selectedStars > i} onSelect={() => {
+            setSelectedStars(i + 1)
+        }} />)}
         <p>{selectedStars} of {totalStars}</p>
     </div>;
 
