6bdb2a3f726da87580f6168f8f20b82135abbd70
Brian Barasa <briannbig@gmail.com>
2023-12-11 11:41:39 +0300

    Add state to starRating compponent to track selected stars

--- src/StarRating/StarRating.js
+++ src/StarRating/StarRating.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa"
 
 const Star = ({ selected = false }) => (
@@ -8,9 +8,10 @@ const createArray = length => [...Array(length)];
 
 export default function StarRating({ totalStars = 5 }) {
 
+    const [selectedStars] = useState(3);
 
     return <div>
-        {createArray(totalStars).map((n, i) => <Star key={i} />)}
+        {createArray(totalStars).map((n, i) => <Star key={i} selected={selectedStars > i} />)}
     </div>;
 
 
